7d788522d1fd1ccf78a24ae32aa75abbec6ea068
unknown <andrebeckpires@gmail.com>
2023-06-03 00:35:56 -0300

    handling errors

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-function fetching() {
-    fetch("http://api.weatherapi.com/v1/current.json?key=a5aed5ac433c4f10af021932230306&q=Sao Paulo&aqi=no")
-        .then((response) => response.json())
-        .then((data) => console.log(data))// output will be the required data
-        .catch((error) => console.log(error))
-}
 
-function test(){
+
+function fetching(){
     var input = document.getElementById("locationSelection").value;
     var requestURL = "http://api.weatherapi.com/v1/current.json?key=a5aed5ac433c4f10af021932230306&q=" + input + "&aqi=no";
     console.log(requestURL);
     fetch(requestURL)
         .then((response) => response.json())
         .then(data => {
-            displayTemp(data)
+            displayTemp(data,true)
           })
-        .catch((error) => console.log(error));
+        .catch(error => {
+            displayTemp("Location not found",false)
+        });
 }
 
-function displayTemp(temp){
+function displayTemp(temp, found){
     var pElement = document.getElementById("temp");
-    pElement.innerHTML = temp.current.temp_c + ' °C <p>Condition: ' + temp.current.condition.text + '</p>';
-    var icon = document.createElement('img');
-    icon.id = "condition"
-    icon.src = "https:" + temp.current.condition.icon
-    pElement.appendChild(icon)
+    if(found == true)
+    {
+        pElement.innerHTML = temp.current.temp_c + ' °C <p>Condition: ' + temp.current.condition.text + '</p>';
+        var icon = document.createElement('img');
+        icon.id = "condition"
+        icon.src = "https:" + temp.current.condition.icon
+        pElement.appendChild(icon)
+    }
+    else{
+        pElement.innerText = temp
+    }
+
 
 }
